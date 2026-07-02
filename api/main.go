6d189120ce62b91ea75b1209f09ed184a20aa2ac
manuelbamise <etobi1930@gmail.com>
2026-07-02 17:01:31 +0100

    chore(api/main):routine save

--- api/main.go
+++ api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,15 +13,14 @@ import (
 
 func main() {
 
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	_, err := db.InitDB()
 	if err != nil {
 		log.Fatalf("failed to init database: %v", err)
 	}
-
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware)
-
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
