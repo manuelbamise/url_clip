9e14d96d6643003a3215bfd9dcfb52d652ce6893
manuelbamise <etobi1930@gmail.com>
2026-07-04 22:13:10 +0100

    chore(scripts): routine save again

--- api/v1/service/url.service.go
+++ api/v1/service/url.service.go
@@ -9,23 +9,30 @@ import (
 )
 
 func CreateUrl(input_url string, database *gorm.DB) (*db.Url, error) {
-	code, err := utils.GenerateCode(5)
-	if err != nil {
-		return nil, err
-	}
+	for i := 0; i < 10; i++ {
+		code, err := utils.GenerateCode(5)
+		if err != nil {
+			return nil, err
+		}
 
-	url := db.Url{
-		Url_link: input_url,
-		Url_code: code,
-	}
+		url := db.Url{
+			Url_link: input_url,
+			Url_code: code,
+		}
 
-	result := database.Create(&url)
-	return &url, result.Error
+		result := database.Create(&url)
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+			continue
+		}
+
+		return &url, result.Error
+	}
+	return nil, errors.New("failed to generate new code")
 }
 
 func GetAllUrls(database *gorm.DB) ([]db.Url, error) {
 	var urls []db.Url
-	database.Find(&urls, &db.Url{})
+	database.Find(&urls, &urls)
 	return urls, nil
 }
 
@@ -33,6 +40,9 @@ func GetUrlByUrlCode(database *gorm.DB, url_code string) (*db.Url, error) {
 	var url db.Url
 
 	result := database.Where("url_code=?", url_code).First(&url)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, result.Error
+	}
 
 	return &url, result.Error
 }
@@ -41,14 +51,14 @@ func GetUrlByUrlLink(database *gorm.DB, input_url string) {
 
 }
 
-func UrlExistsByCode(database *gorm.DB, url_code string) (bool, error) {
-	var url db.Url
-	var count int64
+// func UrlExistsByCode(database *gorm.DB, url_code string) (bool, error) {
+// 	var url db.Url
+// 	var count int64
 
-	result := database.Model(&url).Where("url_code=?", url_code).Count(&count)
-	if result.Error == nil {
-		return true, errors.New("Record already exists")
-	}
+// 	result := database.Model(&url).Where("url_code=?", url_code).Count(&count)
+// 	if result.Error == nil {
+// 		return true, errors.New("Record already exists")
+// 	}
 
-	return false, nil
-}
+// 	return false, nil
+// }
