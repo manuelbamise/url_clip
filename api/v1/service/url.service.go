77c91f5c70468017d9ca66a627ec5fc11299ea20
manuelbamise <etobi1930@gmail.com>
2026-07-04 08:33:36 +0100

    stopped(urlService): creating a loop to check if a code has been assigned to a record and returning an error if true

--- api/v1/service/url.service.go
+++ api/v1/service/url.service.go
@@ -1,6 +1,8 @@
 package v1Services
 
 import (
+	"errors"
+
 	"github.com/manuelbamise/url_clip/utils"
 	"github.com/manuelbamise/url_clip/v1/db"
 	"gorm.io/gorm"
@@ -25,4 +27,31 @@ func GetAllUrls(database *gorm.DB) ([]db.Url, error) {
 	var urls []db.Url
 	database.Find(&urls, &db.Url{})
 	return urls, nil
+}
+
+func GetUrlByUrlCode(database *gorm.DB, url_code string) (*db.Url, error) {
+	var url db.Url
+
+	result := database.Where("url_code=?", url_code).First(&url)
+
+	return &url, result.Error
+}
+
+func GetUrlByUrlLink(database *gorm.DB, input_url string) {
+
+}
+
+func UrlExistsByCode(database *gorm.DB, url_code string) error {
+	var url db.Url
+
+	result := database.Where("url_code=?", url_code).First(&url)
+	if result.Error == nil {
+		return errors.New("Record already exists")
+	}
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	return result.Error
 }
