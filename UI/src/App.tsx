11ef71cc923cabb91c2d75af60aa65b6aa30b599
manuelbamise <etobi1930@gmail.com>
2026-07-06 14:39:01 +0100

    routine: routine save

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="font-bold text-center mt-4">Hello to URL Clip</h1>
